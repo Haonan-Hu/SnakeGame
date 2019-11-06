ffa04c0b210576b980df66cd8153071a9f0b0050
Haonan Hu <hhn97@ku.edu>
2019-11-05 20:08:38 -0600

    create files for snake game

---
+++ snake.js
@@ -0,0 +1,17 @@
+var gameBoard = document.getElementById('board'); //Get DOM object
+if (canvas.getContext) //check availability of canvas
+{
+  var ctx = canvas.getContext('2d'); //return context of canvas
+
+  var grind = 20;
+  var count = 0;
+
+  var snake
+  {
+    //initialize snake position on the board
+    x: 100, //initial x position
+    y: 100; //initial y position
+
+    //initialize snake moving speed
+    xSpeed:
+}
