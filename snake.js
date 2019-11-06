1b2b7988d15d2767d42ce0eba4bde934834fbd70
Haonan Hu <hhn97@ku.edu>
2019-11-05 20:31:06 -0600

    Done variable declearing and ready to implement game itself

--- snake.js
+++ snake.js
@@ -3,15 +3,49 @@ if (canvas.getContext) //check availability of canvas
 {
   var ctx = canvas.getContext('2d'); //return context of canvas
 
-  var grind = 20;
+  var grid = 10; // moving speed variable
   var count = 0;
 
+  //snake object
   var snake
   {
     //initialize snake position on the board
     x: 100, //initial x position
-    y: 100; //initial y position
+    y: 100, //initial y position
 
     //initialize snake moving speed
-    xSpeed:
+    xSpeed: grid,
+    ySpeed: 0,
+
+    //initial size of snake
+    size: 3,
+
+    //snake body, array of grid
+    body: []
+  };
+
+  //berry object
+  var berry
+  {
+    //initialize first berry location
+    x: 250,
+    y: 400
+  };
+
+  //function for generating random number with lower and upper bound
+  //WorkSited: https://stackoverflow.com/a/1527820/2124254
+  random = (min, max)=>
+  {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  //start implementation of game
+  game = ()=>
+  {
+    //tell browser that you wish to perform an animation
+    //WorkSited: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+    requestAnimationFrame(game);
+    
 }
